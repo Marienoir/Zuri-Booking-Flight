e2008c5edec7fd7f52a571562658333197e0d220
marienoir <okosunmaryeghonghon@gmail.com>
2022-06-22 19:00:30 +0100

    Implemented CRUD operations

--- models/Flight.js
+++ models/Flight.js
@@ -1,1 +1,9 @@
-exports.exampleModel = [];
+exports.flights= [
+    {
+        id: '8d739d62-3048-47fb-9478-c54c09433c92',
+        title: 'flight to Lagos state',
+        time: '9pm',
+        price: 8290,
+        date: new Date()
+      }
+];
--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -3,7 +3,11 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/flightController');
 
-router.get('/', controller.example)
+router.post('/flight', controller.addFlight);
+router.get('/flight', controller.getFlights);
+router.get('/flight/:id', controller.getFlightById);
+router.put('/flight/:id', controller.updateFlightById);
+router.delete('/flight/:id', controller.deleteFlightById);
 
 module.exports = router;
 
